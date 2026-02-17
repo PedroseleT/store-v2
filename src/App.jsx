1fb59d9de7ad1970e2c7fce8b81ee50238ba55a3
pedroselet <pedrotelesbrito@gmail.com>
2026-02-17 19:06:04 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,30 @@ import { Home } from "./pages/Home";
 import { Product } from "./pages/Product";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer"; // 1. Importe o Footer
+// No seu App.jsx
+import ReviewSystem from "./components/ReviewSystem"; // Importe aqui
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/produto/:id" element={<Product />} />
+          </Routes>
+        </main>
+
+        {/* COLOCANDO AQUI ELE APARECE EM TUDO! */}
+        <ReviewSystem productId="geral" /> 
+        
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
 
 function App() {
   return (
